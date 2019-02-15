3b57a3f57998e8fc36b66251b26ed9472ceba837
Andy Weekes <userthreethreefive@gmail.com>
2019-02-15 14:13:51 -0700

    first commit

---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritence_demonstrator
+{
+    class Person
+    {
+        public string FirstName;
+        public string SecondName;
+
+        public void SayName()
+        {
+            Console.WriteLine(FirstName + " " + SecondName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritence_demonstrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmployee = new Employee();
+            newEmployee.FirstName = "Sample";
+            newEmployee.SecondName = "Student";
+            newEmployee.SayName();
+            Console.ReadKey();
+        }
+    }
+}
